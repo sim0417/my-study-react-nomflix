2305b2372386c4cbb5c8ddbacb801e40751de28c
Eon Jo <34697086+sim0417@users.noreply.github.com>
2020-10-29 22:04:37 +0900

    updated API's

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ Learning REACT and ES6 by building a Movie Discovery App.
 - [ ] TV Shows
 - [ ] Search
 - [ ] Detail
+
+## API Verbs
+
+- [x] Now Playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV)
+- [x] Popular (TV, Movie)
+- [x] Airing Today (TV)
+- [ ] TV Show Detail
+- [ ] Movie Detail
---
+++ src/api.js
@@ -0,0 +1,22 @@
+import * as cdata from "critical";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: cdata.config.MOVIE_API_KEY,
+    language: "en-US",
+  },
+});
+
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upComing: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+};
